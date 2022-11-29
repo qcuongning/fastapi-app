860200c63340000067f71784fda8be20c79d2341
qcuongning <nihnquoccuong@gmail.com>
2022-11-29 16:37:02 +0700

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi import FastAPI, Header, Request, Response
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post('/phuong')
+async def phuongEvent(request: Request):
+    body = await request.body()
+    print(body)
+    return "ok" 
