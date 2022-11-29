d9a2fdd11d02435772ad8967bdc82af19eb1f78b
qcuongning <nihnquoccuong@gmail.com>
2022-11-29 16:50:39 +0700

    rm

--- main.py
+++ main.py
@@ -6,9 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-@app.post('/phuong')
-async def phuongEvent(request: Request):
-    body = await request.body()
-    print(body)
-    return "ok" 
